27ace331fa9e5238a315ddfb8b7ddbe077adb892
Gaurav C <cggaurav@gmail.com>
2019-01-08 15:45:02 +0530

    Bring sentry in there.

--- app/src/workers/email.worker.js
+++ app/src/workers/email.worker.js
@@ -3,9 +3,14 @@ import * as services from '../services'
 import * as utils from '../utils'
 
 const config = require('config')
+const sentry = require('@sentry/node')
+
+sentry.init({ dsn: config.logging.sentry.key, environment: process.env.NODE_ENV })
 
 async function run () {
+    // TODO: Optimize
     const users = await services.User.getByTimezone()
+
     users.forEach(async (user, index) => {
         const ampm = user.config.send_at.split(' ')[1]
         const hour = user.config.send_at.split(' ')[0]
@@ -15,13 +20,17 @@ async function run () {
 
         const diff = dateTimeToSend.diff(utils.moment_tz().tz(timezone), 'minutes')
 
-        console.log(user.email, hour, ampm, timezone, dateTimeToSend.fromNow(), diff, day, user.config[day])
+        console.info(user.email, hour, ampm, timezone, dateTimeToSend.fromNow(), diff, day, user.config[day])
 
         // Same am/pm, and herokyu workers run every 10 minutes
         if (diff >= 0 && diff <= 11 && user.config[day]) {
-            const random_log = await services.Log.getRandom(user)
-            await services.Email.send(user, 'daily', random_log)
-            console.log(`Sent email for ${user.email} at ${dateTimeToSend.fromNow()}, for ${diff} minutes`)
+            try {
+                const random_log = await services.Log.getRandom(user)
+                await services.Email.send(user, 'daily', random_log)
+                console.log(`Sent email for ${user.email} at ${dateTimeToSend.fromNow()}, for ${diff} minutes`)
+            } catch (err) {
+                sentry.captureException(err)
+            }
         }
 
         if (index === users.length - 1) {
