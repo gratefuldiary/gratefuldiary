f5c9cf752f6dd302915763e3fc6d02ee668f4826
Gaurav C <cggaurav@gmail.com>
2019-01-21 12:16:21 +0800

    More 10

--- app/src/workers/email.worker.js
+++ app/src/workers/email.worker.js
@@ -18,6 +18,7 @@ async function run () {
             const hour = user.config.send_at.split(' ')[0] || '08'
             const timezone = user.config.timezone || 'America/New_York'
             const day = utils.moment_tz().tz(timezone).format('dddd').toLowerCase()
+            // Figure out the time to send from user config in their timezone, looks hacky + ugly
             const dateTimeToSend = utils.moment_tz.tz(`${utils.moment_tz().tz(timezone).format('YYYY-MM-DD')} ${hour} ${ampm}`, "YYYY-MM-DD hh A", timezone)
 
             const diff = Math.abs(dateTimeToSend.diff(utils.moment_tz().tz(timezone), 'minutes'))
@@ -26,7 +27,7 @@ async function run () {
                 utils.moment_tz().tz(timezone).format(), diff, day, user.config[day])
 
             // Same am/pm, and herokyu workers run every 10 minutes, give it some leeway
-            if (diff >= 0 && diff <= 15 && user.config[day]) {
+            if (diff >= 0 && diff <= 10 && user.config[day]) {
                 console.log(`In the process of sending email for ${user.email}...`)
                 try {
                     const random_log = await services.Log.getRandom(user)
